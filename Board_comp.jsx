3e2458c06625453acaa6af7f69c07aae77859d70
nottsatsa <tsatsralsainbolor@gmail.com>
2025-02-18 12:47:36 +0800

    Update Board_comp.jsx

--- Board_comp.jsx
+++ Board_comp.jsx
@@ -1,8 +1,1 @@
-export const Board_comp = ({children})=>{
-    return( 
-        <div className="flex flex-col items-center justify-center gap-[40px] border-solid border-black bg-[#ffffff] w-[377px] py-[24px] px-[16px] rounded-[6px]">
-{children}
-        </div>
-    )
 
-}
